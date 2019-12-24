fb237bae44e19b43c6ae7045e1771d5fc88dce8d
Viking-Daniil <golota.ul@mail.ru>
2019-12-24 21:57:16 +0300

    Залил первую лабораторную

---
+++ Student.cpp
@@ -0,0 +1,196 @@
+#include "Student.h"
+#include <conio.h>//для _getch() 
+//#include < string.h > 
+#include <stdio.h>//h-заголовок для функции fflush(stdin), очищающей входной поток
+#include <iostream>
+#include <stdlib.h>//очистка экрана
+#define NUM 7// Количество элементов массива объектов
+#define NUMO 5// Количество элементов массива оценок
+//#include <cstring>
+//#include <string.h>//h-заголовок для работы со строками
+
+using namespace std;
+
+void Student::set_initials(char *data)
+{
+	long int size = strlen(data);
+	delete[] initials;
+	initials = NULL;
+	initials = new char[size + 1];
+	strcpy(initials, data);
+	return;
+}
+void Student::set_score2(int a)
+{
+	int p;
+	cin >> p;
+	A[a] = 0;
+	A[a] = p;
+	return;
+}
+void Student::set_number(char *data)
+{
+	number = 0;
+	number = atoi(data);
+	return;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+void Student::get_initials(void)
+{
+	cout << "ФИО студента: " << initials << "\n";
+	return;
+}
+void Student::get_score2(void)
+{
+
+	cout << "Оценки студента: "  << "\n";
+	for (int i = 0; i < NUMO; i++)
+	{
+		cout<<A[i];
+	}
+	cout << "\n";
+	return;
+}
+void Student::get_number(void)
+{
+	cout << "Номер группы: " << number << "\n";
+	return;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+Student::Student()
+{
+	initials = new char[100];
+	*(initials + 0) = ' ';
+	*(initials + 1) = '\0';
+	for (int i = 0; i < NUMO; i++)
+	{
+		A[i] = 0;
+	}
+	number = 0;
+}
+Student::~Student()
+{
+	delete[] initials;
+	initials = NULL;
+	for (int i = 0; i < NUMO; i++)
+	{
+		A[i] = 0;
+	}
+
+	number = 0;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+istream &operator>>(istream &stream, Student &obj)
+{
+	fflush(stdin);
+	char str[100];
+	int data_number;
+	int count = 0;
+	bool flag = true;
+	cout << "Введите ФИО студента:\n";
+	gets_s(str);
+	obj.set_initials(str);
+	cout << "Введите оценку студента:\n";
+	for (int i = 0; i < NUMO; i++)
+	{
+		obj.set_score2(i);
+	}
+	cout << "Введите номер группы студента:\n";
+	getchar();
+	gets_s(str);
+	obj.set_number(str);
+	return stream;
+}
+ostream &operator<<(ostream &stream, Student &obj)
+{
+	obj.get_initials();
+	obj.get_score2();
+	obj.get_number();
+	return stream;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+char* Student::take_initials(void)
+{
+	return initials;
+}
+char* Student::take_score(int o)
+{
+	char str2[100];
+	_itoa(A[o], str2, 10);
+	return str2;
+}
+int Student::take_number(void)
+{
+	return number;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+void Student::search_by_score(Student obj[])
+{
+	int flag = 0;
+	char str[10];
+	_itoa(2, str, 10); //Выбор оценки
+	for (int i = 0; i < NUM; i++)
+	{
+		for (int j = 0; j < NUMO; j++)
+		{
+			if (strcmp(obj[i].take_score(j), str) == 0)
+			{
+				flag++;
+				cout << "\n"; //<< obj[i];
+				obj[i].get_initials();
+				obj[i].get_number();
+				break;
+			}
+		}
+	}
+	if (flag == 0)
+		cout << "\n" << "Студентов, имеющих хотя бы одну двойку, нет.\n";
+	return;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+void Student::disp(Student obj[])
+{
+	int count = 1;
+	cout << "\t\t\t\ Информация о студентах (не больше " << NUM << " записей):\n\n";
+	for (int i = 0; i < NUM; i++)
+	{
+		if (*obj[i].take_initials() == ' '&&*obj[i].take_score(i) == ' '&&obj[i].take_number() == 0)//Если запись пуста, не производить вывод
+			continue;
+		cout << "----- " << count << " -----\n";
+		count++;
+		cout << obj[i];
+		cout << "\n\n";
+	}
+	return;
+}
+void Student::sortt(Student obj[])
+{
+	for (int j = 0; j < NUM -1; j++)
+	{
+		for (int i = 0; i < NUM - 1; i++)
+		{
+			if ((strcmp(obj[i].take_initials(), obj[i+1].take_initials()) > 0) && (strcmp(obj[i+1].take_initials(), " ") != 0) && (strcmp(obj[i].take_initials(), " ") != 0))
+			{
+				Student p;
+				p = obj[i];
+				obj[i] = obj[i + 1];
+				obj[i + 1] = p;
+			}
+		}
+	}
+	
+}
+Student &Student ::operator= (const Student &obj)
+{
+	long int size = strlen(obj.initials);
+	delete[] initials;
+	initials = NULL;
+	initials = new char[size + 1];
+	strcpy(initials, obj.initials);
+	for (int i = 0; i < 5; i++)
+	{
+		A[i] = obj.A[i];
+	}
+	number = obj.number;
+	return *this;
+}
---
+++ Student.h
@@ -0,0 +1,39 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+#define NUMO 5
+using namespace std;
+class Student
+{
+private:
+
+	char *initials;//Указатель на строку, в которой содержится информация о ФИО
+	int number;//Номер группы
+	int A[NUMO]; //Массив оценок
+
+public:
+
+	void set_initials(char *data);//Ввод
+	void set_number(char *data);
+	void set_score2(int a);
+
+	void get_initials(void);//Вывод 
+	void get_number(void);
+	void get_score2(void);
+	
+	char* take_initials(void);//Получение 
+	char* take_score(int o);
+	int take_number(void);
+
+	friend ostream &operator<<(ostream &os, Student &obj);//Перегрузка
+	friend istream &operator>>(istream &stream, Student &obj);
+	Student& operator= (const Student &drob);
+
+	static void search_by_score(Student obj[]);//Статический метод
+	static void disp(Student obj[]);
+	static void sortt(Student obj[]);
+
+	Student();
+	~Student();
+};
+#endif 
